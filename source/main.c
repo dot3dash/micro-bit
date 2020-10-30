dac1e0422649f3d2261c2b2fcdcafbe96c6da254
dot3dash <jon.skavlid13@gmail.com>
2020-10-30 18:11:01 +0100

    first commit
    Signed-off-by: dot3dash <jon.skavlid13@gmail.com>

---
+++ source/main.c
@@ -0,0 +1,17 @@
+#include "uart.h"
+//#include "accelerometer.h"
+#include "print.h"
+#include <stdint.h>
+
+int main(){
+    uart_init();
+    //accelerometer_init();
+
+    //int16_t x, y, z;
+
+    while(1){
+        
+    }
+
+    return 0;
+}
